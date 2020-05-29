082a2c29c4415d3c53fc18c38e048b2064d7101f
Jalen Straka <ziencck@gmail.com>
2020-05-29 17:53:38 -0400

    More comments!

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,12 +3,15 @@ var db = require("../models");
 module.exports = function(app) {
     // GET route for getting all of clubs
     app.get("/api/movieclubs/", function(req, res) {
+        // grabbing the MovieClub table and finding all
       db.MovieClubs.findAll({}).then(function(dbClubs) {
+        //   Then creating a json with the results
           res.json(dbClubs);
         });
     });
     // adding to clubs
     app.post("/api/movieclubs", function(req, res){
+        // Creatings the MovieClubs table if it doesn't exist
         db.MovieClubs.create(req.body).then(function(dbClubs){
             res.json(dbClubs);
         });
@@ -19,7 +22,7 @@ module.exports = function(app) {
             res.json(dbAdd);
         })
     });
-    // Grabbing everythin from the Search database
+    // Grabbing everything from the Search database
     app.get("/api/add", function(req, res) {
         db.Search.findAll({}).then(function(dbSearch){
             res.json(dbSearch);
